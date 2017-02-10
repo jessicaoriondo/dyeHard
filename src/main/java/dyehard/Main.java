62ddbf7225530fd792f298f0275f605d760d5414
jessicao <jessicao_36@yahoo.com>
2017-02-10 09:45:31 +0000

    first commit

---
+++ src/main/java/dyehard/Main.java
@@ -0,0 +1,10 @@
+package dyehard;
+import Engine.GameWindow;
+import dyeHardProceduralAPI.DHProceduralAPI;
+import dyeHardProceduralAPI.KeysEnum;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+		(new Run()).startProgram();
+	}
+}
---
+++ src/main/java/dyehard/Run.java
@@ -0,0 +1,13 @@
+package dyehard;
+import Engine.GameWindow;
+
+/**
+ * @author vuochnin
+ *
+ */
+public class Run extends GameWindow{
+
+	public Run(){
+		setRunner(new Demo0());
+	}
+}
